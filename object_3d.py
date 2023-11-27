16a73012660a7c966025cbf7825363db98c28734
EmreAliya <70642795+EmreAliya@users.noreply.github.com>
2023-11-27 17:58:55 -0500

    Add files via upload

---
+++ object_3d.py
@@ -0,0 +1,95 @@
+import pygame as pg
+from matrix_functions import *
+from numba import njit
+import numpy as np
+from Matrix import *
+
+def rotate_z(point, theta):
+    x, y, z = point
+    cos_theta = np.cos(theta)
+    sin_theta = np.sin(theta)
+
+    new_x = x * cos_theta - y * sin_theta
+    new_y = x * sin_theta + y * cos_theta
+    new_z = z  # No change along the Z-axis
+
+    return (new_x, new_y, new_z)
+
+# Example usage:
+original_point = (1, 0, 0)
+rotated_point = rotate_z(original_point, np.pi / 4)  # Rotate by 45 degrees
+print(rotated_point)
+
+
+@njit(fastmath=True)
+def any_func(arr, a, b):
+    return np.any((arr == a) | (arr == b))
+
+
+class Object3D:
+    def __init__(self, render, vertices='', faces=''):
+        self.render = render
+        self.vertices = np.array(vertices)
+        self.faces = faces
+        self.translate([0.0001, 0.0001, 0.0001])
+
+        self.font = pg.font.SysFont('Arial', 30, bold=True)
+        self.color_faces = [(pg.Color('orange'), face) for face in self.faces]
+        self.movement_flag, self.draw_vertices = True, False
+        self.label = ''
+
+    def draw(self):
+        self.screen_projection()
+        self.movement()
+
+    def movement(self):
+        if self.movement_flag:
+            self.rotate_y(-(pg.time.get_ticks() % 0.005))
+
+    def screen_projection(self):
+        vertices = self.vertices @ self.render.camera.camera_matrix()
+        vertices = vertices @ self.render.projection.projection_matrix
+        vertices /= vertices[:, -1].reshape(-1, 1)
+        vertices[(vertices > 2) | (vertices < -2)] = 0
+        vertices = vertices @ self.render.projection.to_screen_matrix
+        vertices = vertices[:, :2]
+
+        for index, color_face in enumerate(self.color_faces):
+            color, face = color_face
+            polygon = vertices[face]
+            if not any_func(polygon, self.render.H_WIDTH, self.render.H_HEIGHT):
+                pg.draw.polygon(self.render.screen, color, polygon, 1)
+                if self.label:
+                    text = self.font.render(self.label[index], True, pg.Color('white'))
+                    self.render.screen.blit(text, polygon[-1])
+
+        if self.draw_vertices:
+            for vertex in vertices:
+                if not any_func(vertex, self.render.H_WIDTH, self.render.H_HEIGHT):
+                    pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 2)
+
+    def translate(self, pos):
+        self.vertices = self.vertices @ translate(pos)
+
+    def scale(self, scale_to):
+        self.vertices = self.vertices @ scale(scale_to)
+
+    def rotate_x(self, angle):
+        self.vertices = self.vertices @ rotate_x(angle)
+
+    def rotate_y(self, angle):
+        self.vertices = self.vertices @ rotate_y(angle)
+
+    def rotate_z(self, angle):
+        self.vertices = self.vertices @ rotate_z(angle)
+
+
+class Axes(Object3D):
+    def __init__(self, render):
+        super().__init__(render)
+        self.vertices = np.array([(0, 0, 0, 1), (1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
+        self.faces = np.array([(0, 1), (0, 2), (0, 3)])
+        self.colors = [pg.Color('red'), pg.Color('green'), pg.Color('blue')]
+        self.color_faces = [(color, face) for color, face in zip(self.colors, self.faces)]
+        self.draw_vertices = False
+        self.label = 'XYZ'
